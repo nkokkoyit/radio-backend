b7064e058143e9f4aa115a56fc5a6522f0950a63
nkokkoyit <trantatthang220893@gmail.com>
2020-05-27 01:20:05 +0700

    day1

--- src/main/java/com/baeldung/crud/controllers/UserController.java
+++ src/main/java/com/baeldung/crud/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.baeldung.crud.controllers;
 
-import javax.validation.Valid;
-
+import com.baeldung.crud.entities.User;
+import com.baeldung.crud.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,55 +10,64 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.baeldung.crud.entities.User;
-import com.baeldung.crud.repositories.UserRepository;
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
-    
+
     private final UserRepository userRepository;
 
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    
-    @GetMapping("/signup")
-    public String showSignUpForm(User user) {
-//        return "add-user";
+
+    @GetMapping("/signin")
+    public String showLoginPage() {
         return "signin";
     }
-    
+
+    @PostMapping("/signin")
+    public String signin(User user) {
+        if (user != null && user.getName().equalsIgnoreCase("admin")) return "add-user";
+        return "signin";
+    }
+
+    @GetMapping("/signup")
+    public String showSignUpForm(User user) {
+        return "add-user";
+    }
+
     @PostMapping("/adduser")
     public String addUser(@Valid User user, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "add-user";
         }
-        
+
         userRepository.save(user);
         model.addAttribute("users", userRepository.findAll());
         return "index";
     }
-    
+
     @GetMapping("/edit/{id}")
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         model.addAttribute("user", user);
         return "update-user";
     }
-    
+
     @PostMapping("/update/{id}")
     public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result, Model model) {
         if (result.hasErrors()) {
             user.setId(id);
             return "update-user";
         }
-        
+
         userRepository.save(user);
         model.addAttribute("users", userRepository.findAll());
         return "index";
     }
-    
+
     @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") long id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
